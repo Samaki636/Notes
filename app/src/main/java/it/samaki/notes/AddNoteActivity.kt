07810f80ae2ba667ac5a2d1e2debee5184def22a
Samaki01 <andrea.quaglia01@gmail.com>
2024-05-03 14:29:18 +0200

    Update date format

--- app/src/main/java/it/samaki/notes/AddNoteActivity.kt
+++ app/src/main/java/it/samaki/notes/AddNoteActivity.kt
@@ -43,7 +43,7 @@ class AddNoteActivity : AppCompatActivity() {
                 Toast.makeText(this, getString(R.string.toast_text), Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val formatter = SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.getDefault())
 
             note = Note()
             note.title = title
